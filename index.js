8a7c5b653731a32a847e35e53829ca08f6d5224a
Kaeland Chatman <kaeland1@gmail.com>
2018-08-11 01:59:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === 'I love you, Grandma.') {
-    return ''
+    return 'I love you, too.'
   } else if (string.toUpperCase() === uppercase) {
     return 'YES INDEED!'
   } else if (string.toLowerCase() === lowercase) {
